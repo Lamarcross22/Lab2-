23b0299687be2b3bb19b0d8701fec290eddc224a
Lamarcross22 <41022434+Lamarcross22@users.noreply.github.com>
2018-07-17 13:00:31 -0400

    day 1 demo

---
+++ Grand  circus/Lab1Part1/Lab1Part1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //first list of numbers
+            bool repeat = true;
+            while(repeat)
+            {
+
+                Console.WriteLine("please enter first number");
+                int userInput1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("please enter second number");
+                int userInput2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("please third number");
+                int userInput3 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("please enter fourth number");
+                int userInput4 = int.Parse(Console.ReadLine());
+
+                int inputSet1 = userInput1 + userInput3;
+                int inputSet2 = userInput2 + userInput4;
+
+                //checkes to see if the sets equal each other or not
+                if (inputSet1 == inputSet2)
+                {
+                    Console.WriteLine("THESE NUMBERS EQUAL");
+                }
+                else
+                {
+                    Console.WriteLine("THESE NUMBERS DONT MATCH");
+                }
+
+                Console.WriteLine("Would you like to try again? Type Y or N.");
+                string userResponse = Console.ReadLine();
+
+                if (userResponse == "Y" || userResponse == "y")
+                {
+                    repeat = true;
+                }
+                else if (userResponse == "N" || userResponse == "n")
+                {
+                    repeat = false;
+                }
+
+            }
+        }
+    }
+}
---
+++ Grand  circus/adding two numbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this program will read two numbers, then show the sum
+            //of those numbers
+
+            // input
+            int input1;
+            int input2;
+            Console.WriteLine("please enter a number");
+           input1= int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter a number");
+            input2 = int.Parse(Console.ReadLine());
+
+
+            // processing
+
+            int result = input1 + input2;
+
+
+            //Output
+
+
+            Console.WriteLine($"The result of adding {input1} and {input2} is {result}");
+
+
+
+        }
+    }
+}
